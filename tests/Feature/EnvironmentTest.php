6682433f938e8b368a0cc523e1838d2e354b930e
Labib Makarim <labibmakarim197@gmail.com>
2024-08-13 15:08:40 +0700

    Mencoba unittest dan env

---
+++ tests/Feature/EnvironmentTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class EnvironmentTest extends TestCase
+{
+    public function testGetEnvironment()
+    {
+        $github = env('GITHUB');
+        self::assertEquals("lbbmkrm", $github);
+    }
+
+    public function testDefaultEnv()
+    {
+        $author = env('author', 'lbbmkrm');
+        self::assertEquals("lbbmkrm", $author);
+    }
+}
