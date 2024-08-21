8fded035811ab080dc86787be0112361facec86b
Labib Makarim <labibmakarim197@gmail.com>
2024-08-21 20:35:29 +0700

    Redirect

---
+++ app/Http/Controllers/RedirectController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\RedirectResponse;
+
+class RedirectController extends Controller
+{
+    public function redirectTo(): string
+    {
+        return "Redirect to";
+    }
+
+    public function redirectFrom(): RedirectResponse
+    {
+        return redirect('/redirect/to');
+    }
+
+    public function redirectName(): RedirectResponse
+    {
+        return redirect()->route("redirect-hello", [
+            "name" => "anto"
+        ]);
+    }
+
+    public function redirectHello(string $name): string
+    {
+        return "Hello $name";
+    }
+
+    public function redirectAway(): RedirectResponse
+    {
+        return redirect()->away("https://github.com/lbbmkrm");
+    }
+}
